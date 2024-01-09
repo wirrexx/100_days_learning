4f72e7ce44b898540ed807bf5ef3336f61e8b315
Wisam <wirrexx@gmail.com>
2024-01-09 19:23:16 +0100

    test

---
+++ Caesars Cipher/Caesars_Cipher.py
@@ -0,0 +1,11 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
+            'v', 'x', 'y', 'z', 'å', 'ä','ö']
+test_text = 'ABC'
+
+def encryption(text, shift):
+    
+
+
+while True:
+    
