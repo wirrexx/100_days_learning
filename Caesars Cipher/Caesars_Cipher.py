252450c27b6ee73c656b84357e209ee79eb16087
Wisam <wirrexx@gmail.com>
2024-01-09 19:35:16 +0100

    encryption done

--- Caesars Cipher/Caesars_Cipher.py
+++ Caesars Cipher/Caesars_Cipher.py
@@ -1,11 +1,18 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
             'v', 'x', 'y', 'z', 'å', 'ä','ö']
-test_text = 'ABC'
+
+test_text = 'abc'.lower()
 
 def encryption(text, shift):
-    
+    encrypted = ''
+    for char in text: 
+        position = alphabet.index(char)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        encrypted += new_letter
+    return encrypted
 
-
-while True:
-    
+        
+        
+print(encryption(test_text, 5))
